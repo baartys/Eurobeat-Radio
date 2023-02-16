16d8950923979d5cc0f0326cc4ce4a2450f5aa83
Baartys_ <102672880+baartys@users.noreply.github.com>
2023-02-16 07:20:49 +0100

    The Basic Bot File
    Just the demo version for the initial work of the bot

---
+++ radio no token.py
@@ -0,0 +1,34 @@
+import discord
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+client= commands.Bot(command_prefix="er!", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print('Bot is running!')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="er!play / er!join"))
+
+
+@client.command(aliases=['p', 'pla', 'join', 'j'])
+async def play(ctx, url: str = 'http://stream.laut.fm/eurobeat'):
+    channel = ctx.message.author.voice.channel
+    global player
+    try:
+        player = await channel.connect()
+    except:
+        pass
+    player.play(FFmpegPCMAudio('http://stream.laut.fm/eurobeat'))
+    await ctx.send("**Started playing!**")
+
+
+@client.command(aliases=['s', 'sto'])
+async def stop(ctx):
+    channel = ctx.message.author.voice.channel
+    player.stop()
+    channel.disconnect()
+    await ctx.send("**Stopped playing!**")
+
+@client.command(aliases=['i'])
+async def info(ctx):
+    await ctx.send("**Info about the Eurobeat FM**\nhttps://laut.fm/eurobeat")
+client.run("TOKEN")
